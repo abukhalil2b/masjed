a62fbd2741b6d8ae4df2ad0cccb664f6f657b467
abukhalil2b <abukhalil2b>
2022-01-07 21:44:26 +0400

    f

--- resources/views/layouts/sb_admin.blade.php
+++ resources/views/layouts/sb_admin.blade.php
@@ -246,34 +246,7 @@
 	                        </li>
 
 	                        <!-- Nav Item - Alerts -->
-	                        <li class="nav-item dropdown no-arrow mx-1">
-	                            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
-	                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-	                                <i class="fas fa-bell fa-fw"></i>
-	                                <!-- Counter - Alerts -->
-	                                <span class="badge badge-danger badge-counter">0</span>
-	                            </a>
-	                            <!-- Dropdown - Alerts -->
-	                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-	                                aria-labelledby="alertsDropdown">
-	                                <h6 class="dropdown-header">
-	                                    ...
-	                                </h6>
-	                                <a class="dropdown-item d-flex align-items-center" href="#">
-	                                    <div class="mr-3">
-	                                        <div class="icon-circle bg-primary">
-	                                            <i class="fas fa-file-alt text-white"></i>
-	                                        </div>
-	                                    </div>
-	                                    <div>
-	                                        <div class="small text-gray-500">...</div>
-	                                        <span class="font-weight-bold">...</span>
-	                                    </div>
-	                                </a>
-	
-	                                <a class="dropdown-item text-center small text-gray-500" href="#">..</a>
-	                            </div>
-	                        </li>
+	                        
 
 	                        
 
@@ -284,7 +257,7 @@
 	                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
 	                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 	                                
-	                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span>
+	                                <span class="mr-2 d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span>
 	                                
 	                            </a>
 	                            <!-- Dropdown - User Information -->
